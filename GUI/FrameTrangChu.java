bbfd68eaa5d546ad3c07261fdf5053f24b65c10b
Trần Bình Minh <c3lttrong.2a3.tbminh@gmail.com>
2024-11-08 12:04:37 +0700

    Update icon

--- GUI/FrameTrangChu.java
+++ GUI/FrameTrangChu.java
@@ -1,3 +1,4 @@
+
 package GUI;
 
 import java.awt.*;
@@ -7,199 +8,179 @@ import java.awt.event.ActionListener;
 
 public class FrameTrangChu extends JFrame {
 
-	private static final long serialVersionUID = 1L;
-	private FrameQuanLyKhachHang frameQLKH;
-	private FrameQuanLySanPham frameQLSP;
-	private FrameQuanLyHoaDon frameQLHD;
-	private FrameQuanLyNhanVien frameQLNV;
-	public String maNV, chucVu;
-	/**
-	 * Launch the application.
-	 */
-//	public static void main(String[] args) {
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					FrameTrangChu frame = new FrameTrangChu(maNV, chucVu);
-//					frame.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-//	}
+    private static final long serialVersionUID = 1L;
+    private FrameQuanLyKhachHang frameQLKH;
+    private FrameQuanLySanPham frameQLSP;
+    private FrameQuanLyHoaDon frameQLHD;
+    private FrameQuanLyNhanVien frameQLNV;
+    public String maNV, chucVu;
 
-	/**
-	 * Create the frame.
-	 */
-	public FrameTrangChu(String maNV, String chucVu) {
-		setIconImage(Toolkit.getDefaultToolkit().getImage("image\\store.png"));
-		setTitle("Quản lý bán hàng");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1003, 630);
-		setLocationRelativeTo(null);
-		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		getContentPane().setLayout(new BorderLayout(0, 0));
-		
-		JLabel background_image = new JLabel("");
-		background_image.setIcon(new ImageIcon("image\\market.jpg"));
-		getContentPane().add(background_image);
-		
-		JMenuBar menuBar = new JMenuBar();
-		getContentPane().add(menuBar, BorderLayout.NORTH);
-		menuBar.setBackground(new Color(192, 192, 192));
-		
-		JMenu btnHeThong = new JMenu("Hệ thống");
-		btnHeThong.setIcon(new ImageIcon("icon\\system.png"));
-		menuBar.add(btnHeThong);
-		
-		JMenuItem btnTrangChu = new JMenuItem("Trang chủ");
-		btnTrangChu.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				getContentPane().removeAll();
-				getContentPane().add(menuBar, BorderLayout.NORTH);
-		        getContentPane().add(background_image, BorderLayout.CENTER);
-		        revalidate();
-		        repaint();
-			}
-		});
-		btnTrangChu.setIcon(new ImageIcon("icon\\homepage.png"));
-		btnHeThong.add(btnTrangChu);
-		
-		JMenuItem btnChinhSua = new JMenuItem("Chỉnh sửa");
-		btnChinhSua.setIcon(new ImageIcon("icon\\editprofile.png"));
-		btnHeThong.add(btnChinhSua);
-		
-		JMenuItem btnDangXuat = new JMenuItem("Đăng xuất");
-		btnDangXuat.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				int a = JOptionPane.showConfirmDialog(FrameTrangChu.this, "Bạn có muốn đăng xuất ứng dụng không?", "Lựa chọn", JOptionPane.YES_NO_OPTION);
-		        if (a == JOptionPane.YES_OPTION) {
-		        	dispose();
-			        FrameDangNhap Framelogin = new FrameDangNhap();
-			        Framelogin.setVisible(true); 
-		        }
-				
-			}
-		});
-		btnDangXuat.setIcon(new ImageIcon("icon\\signout.png"));
-		btnHeThong.add(btnDangXuat);
-		
-		JMenuItem btnThoat = new JMenuItem("Thoát");
-		btnThoat.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				int a = JOptionPane.showConfirmDialog(FrameTrangChu.this, "Bạn có muốn thoát ứng dụng không?", "Lựa chọn", JOptionPane.YES_NO_OPTION);
-		        if (a == JOptionPane.YES_OPTION) {
-		        	dispose();
-		        }
-			}
-		});
-		btnThoat.setIcon(new ImageIcon("icon\\exit.png"));
-		btnHeThong.add(btnThoat);
-		
-		JMenu btnDanhMuc = new JMenu("Danh mục");
-		btnDanhMuc.setIcon(new ImageIcon("icon\\catalog.png"));
-		menuBar.add(btnDanhMuc);
-		
-		JMenuItem btnKhachHang = new JMenuItem("Khách hàng");
-		btnKhachHang.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frameQLKH = new FrameQuanLyKhachHang();
-				getContentPane().removeAll();
-				getContentPane().add(menuBar, BorderLayout.NORTH);
-				getContentPane().add(frameQLKH, BorderLayout.CENTER);
-				revalidate();
-				repaint();
-			}
-		});
-		
-		btnKhachHang.setIcon(new ImageIcon("icon\\customer.png"));
-		btnDanhMuc.add(btnKhachHang);
-		
-		JMenuItem btnNhanVien = new JMenuItem("Nhân viên");
-		btnNhanVien.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frameQLNV = new FrameQuanLyNhanVien();
-				getContentPane().removeAll();
-				getContentPane().add(menuBar, BorderLayout.NORTH);
-				getContentPane().add(frameQLNV, BorderLayout.CENTER);
-				revalidate();
-				repaint();
-			}
-		});
-		btnNhanVien.setIcon(new ImageIcon("icon\\employee.png"));
-		btnDanhMuc.add(btnNhanVien);
-		
-		JMenuItem btnSanPham = new JMenuItem("Sản phẩm");
-		btnSanPham.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frameQLSP = new FrameQuanLySanPham();
-				getContentPane().removeAll();
-				getContentPane().add(menuBar, BorderLayout.NORTH);
-				getContentPane().add(frameQLSP, BorderLayout.CENTER);
-				revalidate();
-				repaint();
-			}
-		});
-		btnSanPham.setIcon(new ImageIcon("icon\\product.png"));
-		btnDanhMuc.add(btnSanPham);
-		
-		JMenuItem btnHoaDon = new JMenuItem("Hóa đơn");
-		btnHoaDon.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frameQLHD = new FrameQuanLyHoaDon();
-				getContentPane().removeAll();
-				getContentPane().add(menuBar, BorderLayout.NORTH);
-				getContentPane().add(frameQLHD, BorderLayout.CENTER);
-				revalidate();
-				repaint();
-			}
-		});
-		btnHoaDon.setIcon(new ImageIcon("icon\\bill.png"));
-		btnDanhMuc.add(btnHoaDon);
-		
-		JMenu btnThongKe = new JMenu("Thống kê");
-		btnThongKe.setIcon(new ImageIcon("icon\\static.png"));
-		menuBar.add(btnThongKe);
-		
-		JMenuItem mntmNewMenuItem_6 = new JMenuItem("Doanh thu");
-		mntmNewMenuItem_6.setIcon(new ImageIcon("icon\\static_revenue.png"));
-		btnThongKe.add(mntmNewMenuItem_6);
-		
-		JMenuItem mntmNewMenuItem_7 = new JMenuItem("Khách hàng");
-		mntmNewMenuItem_7.setIcon(new ImageIcon("icon\\static_customer.png"));
-		btnThongKe.add(mntmNewMenuItem_7);
-		
-		JMenuItem mntmNewMenuItem_8 = new JMenuItem("Sản phẩm");
-		mntmNewMenuItem_8.setIcon(new ImageIcon("icon\\static_product.png"));
-		btnThongKe.add(mntmNewMenuItem_8);
-		
-		JMenu btnInfo = new JMenu("Thông tin");
-		btnInfo.setIcon(new ImageIcon("icon\\info.png"));
-		menuBar.add(btnInfo);
-		
-		JMenuItem btnThongTin = new JMenuItem("Thông tin phần mềm");
-		btnThongTin.setIcon(new ImageIcon("icon\\info_phanmem.png"));
-		btnInfo.add(btnThongTin);
-		
-		JMenuItem btnDuongDan = new JMenuItem("Đường dẫn");
-		btnDuongDan.setIcon(new ImageIcon("icon\\link.png"));
-		btnInfo.add(btnDuongDan);
-		
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(255, 255, 255));
-		menuBar.add(panel);
-		panel.setLayout(null);
-		
-		JLabel lblUsername = new JLabel(chucVu + " " + maNV);
-		lblUsername.setIcon(new ImageIcon("icon\\user.png"));
-		lblUsername.setBounds(1004, 0, 128, 39);
-		panel.add(lblUsername);
-		
-		if ("Nhân Viên".equals(chucVu)) {
-	        btnNhanVien.setEnabled(false);
-	    }
-		
-		this.setVisible(true);
-	}
+//    public static void main(String[] args) {
+//        EventQueue.invokeLater(new Runnable() {
+//            public void run() {
+//                try {
+//                    FrameTrangChu frame = new FrameTrangChu("NV1", "Quản lý");
+//                    frame.setVisible(true);
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//    }
+
+    public FrameTrangChu(String maNV, String chucVu) {
+        setIconImage(Toolkit.getDefaultToolkit().getImage("image\\store.png"));
+        setTitle("Quản lý bán hàng");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 1003, 630);
+        setLocationRelativeTo(null);
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        BackgroundPanel pnlBackGround = new BackgroundPanel("image\\market.jpg");
+        pnlBackGround.setLayout(new BorderLayout());
+        setContentPane(pnlBackGround);
+
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        menuBar.setBackground(new Color(192, 192, 192));
+
+        JMenu btnHeThong = new JMenu("Hệ thống");
+        btnHeThong.setIcon(new ImageIcon("icon\\system.png"));
+        menuBar.add(btnHeThong);
+
+        JMenuItem btnTrangChu = new JMenuItem("Trang chủ");
+        btnTrangChu.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                pnlBackGround.removeAll();
+                pnlBackGround.add(new BackgroundPanel("image\\market.jpg"), BorderLayout.CENTER);
+                revalidate();
+                repaint();
+            }
+        });
+        btnTrangChu.setIcon(new ImageIcon("icon\\homepage.png"));
+        btnHeThong.add(btnTrangChu);
+
+        JMenuItem btnChinhSua = new JMenuItem("Chỉnh sửa");
+        btnChinhSua.setIcon(new ImageIcon("icon\\editprofile.png"));
+        btnHeThong.add(btnChinhSua);
+
+        JMenuItem btnDangXuat = new JMenuItem("Đăng xuất");
+        btnDangXuat.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int a = JOptionPane.showConfirmDialog(FrameTrangChu.this, "Bạn có muốn thoát ứng dụng không?", "Lựa chọn", JOptionPane.YES_NO_OPTION);
+                if (a == JOptionPane.YES_OPTION) {
+                    dispose();
+                    FrameDangNhap Framelogin = new FrameDangNhap();
+                    Framelogin.setVisible(true);
+                }
+            }
+        });
+        btnDangXuat.setIcon(new ImageIcon("icon\\signout.png"));
+        btnHeThong.add(btnDangXuat);
+
+        JMenuItem btnThoat = new JMenuItem("Thoát");
+        btnThoat.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+        btnThoat.setIcon(new ImageIcon("icon\\exit.png"));
+        btnHeThong.add(btnThoat);
+
+        JMenu btnDanhMuc = new JMenu("Danh mục");
+        btnDanhMuc.setIcon(new ImageIcon("icon\\catalog.png"));
+        menuBar.add(btnDanhMuc);
+
+        JMenuItem btnKhachHang = new JMenuItem("Khách hàng");
+        btnKhachHang.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                frameQLKH = new FrameQuanLyKhachHang();
+                pnlBackGround.removeAll();
+                pnlBackGround.add(frameQLKH, BorderLayout.CENTER);
+                revalidate();
+                repaint();
+            }
+        });
+        btnKhachHang.setIcon(new ImageIcon("icon\\customer.png"));
+        btnDanhMuc.add(btnKhachHang);
+
+        JMenuItem btnNhanVien = new JMenuItem("Nhân viên");
+        btnNhanVien.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                frameQLNV = new FrameQuanLyNhanVien();
+                pnlBackGround.removeAll();
+                pnlBackGround.add(frameQLNV, BorderLayout.CENTER);
+                revalidate();
+                repaint();
+            }
+        });
+        btnNhanVien.setIcon(new ImageIcon("icon\\employee.png"));
+        btnDanhMuc.add(btnNhanVien);
+
+        JMenuItem btnSanPham = new JMenuItem("Sản phẩm");
+        btnSanPham.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                frameQLSP = new FrameQuanLySanPham();
+                pnlBackGround.removeAll();
+                pnlBackGround.add(frameQLSP, BorderLayout.CENTER);
+                revalidate();
+                repaint();
+            }
+        });
+        btnSanPham.setIcon(new ImageIcon("icon\\product.png"));
+        btnDanhMuc.add(btnSanPham);
+
+        JMenuItem btnHoaDon = new JMenuItem("Hóa đơn");
+        btnHoaDon.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                frameQLHD = new FrameQuanLyHoaDon();
+                pnlBackGround.removeAll();
+                pnlBackGround.add(frameQLHD, BorderLayout.CENTER);
+                revalidate();
+                repaint();
+            }
+        });
+        btnHoaDon.setIcon(new ImageIcon("icon\\bill.png"));
+        btnDanhMuc.add(btnHoaDon);
+
+        JMenu menuThongKe = new JMenu("Thống kê");
+        menuThongKe.setIcon(new ImageIcon("icon\\static.png"));
+        menuBar.add(menuThongKe);
+
+        JMenuItem mntmNewMenuItem_6 = new JMenuItem("Doanh thu");
+        mntmNewMenuItem_6.setIcon(new ImageIcon("icon\\static_revenue.png"));
+        menuThongKe.add(mntmNewMenuItem_6);
+
+        JMenuItem mntmNewMenuItem_7 = new JMenuItem("Khách hàng");
+        mntmNewMenuItem_7.setIcon(new ImageIcon("icon\\static_customer.png"));
+        menuThongKe.add(mntmNewMenuItem_7);
+
+        JMenuItem mntmNewMenuItem_8 = new JMenuItem("Sản phẩm");
+        mntmNewMenuItem_8.setIcon(new ImageIcon("icon\\static_product.png"));
+        menuThongKe.add(mntmNewMenuItem_8);
+
+        JMenu menuThongTin = new JMenu("Thông tin");
+        menuThongTin.setIcon(new ImageIcon("icon\\info.png"));
+        menuBar.add(menuThongTin);
+
+        JMenuItem btnThongTin = new JMenuItem("Thông tin phần mềm");
+        btnThongTin.setIcon(new ImageIcon("icon\\info_phanmem.png"));
+        menuThongTin.add(btnThongTin);
+
+        JMenuItem btnDuongDan = new JMenuItem("Đường dẫn");
+        btnDuongDan.setIcon(new ImageIcon("icon\\link.png"));
+        menuThongTin.add(btnDuongDan);
+
+        JPanel panel = new JPanel();
+        panel.setBackground(new Color(255, 255, 255));
+        menuBar.add(panel);
+        panel.setLayout(null);
+
+        JLabel lblUsername = new JLabel(chucVu + " " + maNV);
+        lblUsername.setIcon(new ImageIcon("icon\\user.png"));
+        lblUsername.setBounds(1004, 0, 128, 39);
+        panel.add(lblUsername);
+
+        this.setVisible(true);
+    }
 }
